9c18703713be723f90deedfd6427369ad321ccb2
Julia Meister <juliameister@users.noreply.github.com>
2021-02-06 20:35:52 +0000

    MazeHandler retrieves questions and validates answers for QNodes

--- maze_server/maze_handler/MazeHandler.py
+++ maze_server/maze_handler/MazeHandler.py
@@ -1,4 +1,5 @@
 import pickle
+import re
 
 # from MazeGenerator import Maze
 from MazeGenerator import Maze
@@ -10,26 +11,31 @@ class MazeHandler:
         nx, ny = 10,10  # Maze dimensions (ncols, nrows)
         ix, iy = 0, 0  # Maze entry position
         self.maze = Maze(nx, ny, ix, iy)
-        self.maze.make_maze()
         # self.maze_runner = MazeRunner(self.get_maze())
         # # pickle.dump(self.maze, open("testMaze.pickle", "wb"))
-        # self.maze = pickle.load(open("testMaze.pickle", "rb"))
+        # pickle.dump(self.maze, open("testMaze.pickle", "wb"))
+        self.maze = pickle.load(open("testMaze.pickle", "rb"))
         self.team_locations = {}  # assume no duplicates
 
     # def create_maze(self):
     #     self.maze.make_maze()
 
+
     def get_challenge(self, node):
         pass
+
 
     def get_location(self, team):
         return self.maze.cell_at(*self.team_locations[team])
 
+
     def get_maze(self):
         return self.maze
 
+
     def get_timeout(self, team, node):
         pass
+
 
     # create_team
     def register_team(self, team):
@@ -47,7 +53,7 @@ class MazeHandler:
     #     if code == 4:
     #         self.end_node(team)
     #     if code == 3:
-    #         self.question(team)
+    #         self.question(team, location)
     #     if code == 2:
     #         self.deadend(team)
     #     if code == 1:
@@ -55,21 +61,42 @@ class MazeHandler:
     #     if code == 0:
     #         print("Invalid Move")
 
+
     def set_location(self, location, team):
-        self.team_location.update({team: location})
-        print(self.team_location)
+        self.team_locations.update({team: location})
+
 
     def end_node(self, team):
         print("END NODE")
 
-    def question(self, team):
-        print("Question")
+
+    def question(self, team, location):
+        ##### TODO: Track which questions have already been answered
+        """Returns the question as a string."""
+        return self.maze.get_cell_question(location)
+
+
+    def validate_answer(self, team, answer):
+        answer_formatted = re.sub('[\W_]', '', answer.lower())
+        location = self.get_location(team).get_coordinates()
+        print(location)
+
+        if answer_formatted in self.maze.get_cell_answer(location):
+            ##### TODO: add score
+            return "Correct answer."
+        else:
+            return "Wrong answer."
+
 
     def junction(self, team, possible_moves):
         print("Junction")
         print(possible_moves)
 
+
     def deadend(self, team):
         print("Deadend")
 
 mh = MazeHandler()
+
+print(mh.validate_answer('team1', 'Hu"?MAn'))
+# mh.question()
