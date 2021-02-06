2c92d09e0ebd5ef21209d4e8db22ae4f70117147
ariten <ariten@hotmail.co.uk>
2021-02-06 19:32:49 +0000

    Updating MazeHandler.py

--- maze_server/maze_handler/MazeHandler.py
+++ maze_server/maze_handler/MazeHandler.py
@@ -1,9 +1,58 @@
+from maze_server.maze_handler.MazeGenerator import Maze
+from maze_server.maze_handler.MazeRunner import MazeRunner
+
+
 class MazeHandler:
     def __init__(self):
-        pass
+        self.maze = Maze(100, 100)
+        self.maze_runner = MazeRunner(self.get_maze())
+        self.team_location = {}
+
+    def create_maze(self):
+        self.maze.make_maze()
+
+    def create_team(self, team):
+        self.team_location.update({team: (0, 0)})
 
     def get_location(self, team):
-        pass
+        return self.maze.cell_at(*self.team_location[team])
 
-    def move_player(self, team, direction):
-        pass
+    def move_player(self, direction, team):
+        location = self.get_location(team)
+        response = self.maze_runner.run_direction(location, direction)
+        code = response[0]
+        print(code)
+        self.set_location(response[1].get_coordinates, team)
+        movements = response[2]
+        print(movements)
+        print(response[1].get_coordinates())
+        if code == 4:
+            self.end_node()
+        if code == 3:
+            self.question()
+        if code == 2:
+            self.deadend()
+        if code == 1:
+            self.junction(response[3])
+        if code == 0:
+            print("Invalid Move")
+
+    def set_location(self, location, team):
+        self.team_location.update({team: location})
+        print(self.team_location)
+
+    def get_maze(self):
+        return self.maze
+
+    def end_node(self):
+        print("END NODE")
+
+    def question(self):
+        print("Question")
+
+    def junction(self, possible_moves):
+        print("Junction")
+        print(possible_moves)
+
+    def deadend(self):
+        print("Deadend")
