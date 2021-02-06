e8addc12142e74fb22dd13e819de0d8845449592
Henry Gilbert <ausanc@users.noreply.github.com>
2021-02-06 15:47:27 +0000

    Add quick and dirty setting of team name

--- maze_server/maze/urls.py
+++ maze_server/maze/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('testjson', views.test_json_call, name='test_call'),
+    path('registerteam', views.register_team, name='register_team')
 ]
 
--- maze_server/maze/views.py
+++ maze_server/maze/views.py
@@ -7,11 +7,22 @@ def index(request):
     return render(request, "maze/index.html")
 
 
+def register_team(request):
+    """Takes the user's input of team name and stores it in the session as the user's unique ID."""
+    if request.is_ajax() and request.method == 'POST':
+        team_name = request.POST.get("userInput", "")
+        print("User registered team %s" % team_name)
+        request.session["userID"] = team_name
+        return JsonResponse({"success": True, "teamName": request.session["userID"]})
+    return JsonResponse({"success": False})
+
+
 def test_json_call(request):
     """Used to test a vertical slice of the stack."""
     print(request)
     if request.is_ajax() and request.method == 'POST':
         print(request.body)
         data = request.POST.get("userInput", "")
-        print(data)
+        userID = request.session["userID"]
+        print("User from %s submitted %s" % (userID, data))
     return JsonResponse({"direction": "North", "distance": 10})
