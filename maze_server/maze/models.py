dcdc22733cb8913179de5fabc6a15501698a20eb
Henry Gilbert <ausanc@users.noreply.github.com>
2021-02-06 16:05:30 +0000

    Add team model with team_name field

--- maze_server/maze/models.py
+++ maze_server/maze/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    def __str__(self):
+        return self.team_name
+
+    team_name = models.CharField(max_length=100)
