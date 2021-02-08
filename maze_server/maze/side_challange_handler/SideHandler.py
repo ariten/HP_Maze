75985d4051a72e4c151b103cc8d5d35e7516f3e3
ariten <ariten@hotmail.co.uk>
2021-02-08 17:28:53 +0000

    Adding in images rewards for challenges.

--- maze_server/maze/side_challange_handler/SideHandler.py
+++ maze_server/maze/side_challange_handler/SideHandler.py
@@ -19,27 +19,27 @@ class SideHandler:
     def question_1(self, answer):
         actual_answer = 'rubeus hagrid'
         if answer.lower() == actual_answer:
-            return ["Success", '2p8934u5', 100]
+            return ["Success", '2p8934u5', 100]  # Top Left
         else:
             return ['Incorrect', '', 0]
 
     def question_2(self, answer):
         actual_answer = 'crookshanks'
         if answer.lower() == actual_answer:
-            return ["Success", '2p8956gc', 100]
+            return ["Success", '2p8956gc', 100]  # Bottom Left
         else:
             return ['Incorrect', '', 0]
 
     def question_3(self, answer):
         actual_answer = 'nymphadora'
         if answer.lower() == actual_answer:
-            return ["Success", '2p894des', 100]
+            return ["Success", '2p894des', 100]  # Top Right
         else:
             return ['Incorrect', '', 0]
 
     def question_4(self, answer):
         actual_answer = 'lunalovegood'
         if answer.lower() == actual_answer:
-            return ["Success", '2p89k65c', 100]
+            return ["Success", '2p89k65c', 100]  # Bottom Right
         else:
             return ['Incorrect', '', 0]
